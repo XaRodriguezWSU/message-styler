d5f7f0672fd6f11c58227c055b8a69fa24631ea7
XaRodriguezWSU <xanderrodriguez@mail.weber.edu>
2026-09-02 19:43:37 +0000

    Task B: add -c COUNT with validation

--- style.sh
+++ style.sh
@@ -3,14 +3,16 @@
 
 usage() {
 cat <<TXT
-Usage: ${0##*/} [-h] [-n NAME]
+Usage: ${0##*/} [-h] [-n NAME] [-c COUNT]
 Options:
     -h          Show this help message
     -n NAME     Greet NAME (default: stranger)
+    -c COUNT    Number of greetings (default: 1)
 
 Examples:
-    ${0##*/} -n Xander
     ${0##*/} -h
+    ${0##*/} -n Xander
+    ${0##*/} -n Xander -c 4
 TXT
 }
 
@@ -21,15 +23,25 @@ error() {
 }
 
 name="stranger"
+count=1
 
-while getopts ":hn:" opt
+while getopts ":hn:c:" opt
 do
     case "$opt" in
         h) usage; exit 0 ;;
         n) name="$OPTARG" ;;
+        c) count="$OPTARG" ;;
         \?) error "Unknown option: -$OPTARG" ;;
         :) error "Missing argument for -$OPTARG" ;;
     esac
 done
 
-echo "Hello, $name!"
+if ! [[ "$count" =~ ^[1-9][0-9]*$ ]]
+then
+    error "COUNT must be a postive integer (>=1)."
+fi
+
+for ((i=1; i<=count; i++))
+do
+    echo "Hello, $name!"
+done
